a3ace1ffa24caf385e73c4832ddc551fcc5d9106
Bernard Choi <bchoii@gmail.com>
2022-04-11 23:36:52 +0800

    .

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,5 @@
-pwd
-ls
 curl -s https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/v3.9.14/deps/rabbitmq_management/bin/rabbitmqadmin -o rabbitmqadmin
-ls
+pwd
+ls -lsta
 chmod +x rabbitmqadmin
 ./rabbitmqadmin
