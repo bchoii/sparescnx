6b7ac3396b72970551c8f2471091f465a76ad5a3
Bernard Choi <bchoii@gmail.com>
2022-04-11 23:14:16 +0800

    cannot run apt-get . not root

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,3 @@
-apt-get update
-apt-get install python3.6
-python3 --version
 pwd
 ls
 curl -s https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/v3.9.14/deps/rabbitmq_management/bin/rabbitmqadmin -o rabbitmqadmin
