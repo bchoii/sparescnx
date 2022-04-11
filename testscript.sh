cebf3fcf237c052b4fa5b4f0933b9e7afd5fa1ef
Bernard Choi <bchoii@gmail.com>
2022-04-11 21:40:07 +0800

    chmod

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,5 @@ pwd
 ls
 curl -s https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/v3.9.14/deps/rabbitmq_management/bin/rabbitmqadmin -o rabbitmqadmin
 ls
+chmod +x rabbitmqadmin
 ./rabbitmqadmin
