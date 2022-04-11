8f62141159898a768d3527fed1a18b30faa07a42
Bernard Choi <bchoii@gmail.com>
2022-04-11 21:43:06 +0800

    add python3 env

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ ls
 curl -s https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/v3.9.14/deps/rabbitmq_management/bin/rabbitmqadmin -o rabbitmqadmin
 ls
 chmod +x rabbitmqadmin
+export PATH=${PATH}:/usr/bin/python3
 ./rabbitmqadmin
