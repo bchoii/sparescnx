01c7839cf50bd06afa97e5d39ecf7a3cdbc5f63a
Bernard Choi <bchoii@gmail.com>
2022-04-19 10:19:09 +0800

    use env var

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,4 @@ curl -s https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/v3.9.14/deps/
 chmod +x rabbitmqadmin
 # which python
 # which python3
-./rabbitmqadmin -H nonprod-mq.teleport.sparescnx.com list queues
+./rabbitmqadmin -H ${RABBITMQ_HOSTNAME} -u ${RABBITMQ_USERNAME} -p ${RABBITMQ_PASSWORD} list queues
