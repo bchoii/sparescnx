3378a080956e17c46001010219024407cfd7c22b
Bernard Choi <bchoii@gmail.com>
2022-04-19 10:21:22 +0800

    echo test

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,6 @@ curl -s https://raw.githubusercontent.com/rabbitmq/rabbitmq-server/v3.9.14/deps/
 chmod +x rabbitmqadmin
 # which python
 # which python3
+echo ${RABBITMQ_HOSTNAME}
+echo ${RABBITMQ_USERNAME}
 ./rabbitmqadmin -H ${RABBITMQ_HOSTNAME} -u ${RABBITMQ_USERNAME} -p ${RABBITMQ_PASSWORD} list queues
