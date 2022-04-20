910510a1242917485fbd22cc92c639f34a035459
Bernard Choi <bchoii@gmail.com>
2022-04-20 17:17:05 +0800

    .

--- gocd.py
+++ gocd.py
@@ -20,8 +20,9 @@ for row in data:
   # print(row)
   # print(row['vhost'])
   # print(row['name'])
-  output.write("./rabbitmqadmin -H ${{CLOUD_RABBITMQ_HOSTNAME}} -u ${{CLOUD_RABBITMQ_USERNAME}} -p ${{CLOUD_RABBITMQ_PASSWORD}} --vhost={} purge queue name={}".format(row['vhost'], row['name']))
-  output.write('\n')
+  if ".retry" in row['name']:
+    output.write("./rabbitmqadmin -H ${{CLOUD_RABBITMQ_HOSTNAME}} -u ${{CLOUD_RABBITMQ_USERNAME}} -p ${{CLOUD_RABBITMQ_PASSWORD}} --vhost={} purge queue name={}".format(row['vhost'], row['name']))
+    output.write('\n')
 
 output.close()
 
