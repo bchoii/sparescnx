325cdd2db4633f647423762d9ce28406ee22743e
Bernard Choi <bchoii@gmail.com>
2022-04-20 17:45:22 +0800

    .

--- gocd.sh
+++ gocd.sh
@@ -1,5 +1,11 @@
+pwd
+ls
 curl http://${CLOUD_RABBITMQ_HOSTNAME}:15672/cli/rabbitmqadmin -o rabbitmqadmin && chmod +x rabbitmqadmin
+pwd
+ls
 ./rabbitmqadmin -H ${CLOUD_RABBITMQ_HOSTNAME} -u ${CLOUD_RABBITMQ_USERNAME} -p ${CLOUD_RABBITMQ_PASSWORD} -f raw_json list queues vhost name messages > queues.json
+pwd
+ls
 python3 ./sparescnx/generatePurgeSh.py
 cat purge.sh
 chmod +x purge.sh
