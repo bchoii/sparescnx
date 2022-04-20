40838d8d5308274a783d38495b45ddb258a8dbfe
Bernard Choi <bchoii@gmail.com>
2022-04-20 17:13:17 +0800

    .

--- gocd.sh
+++ gocd.sh
@@ -1,6 +1,6 @@
 python3 ./sparescnx/gocd.py
 ls sparescnx
-cat sparescnx/purge.sh
+cat purge.sh
 # cat queues.json
 # ./rabbitmqadmin --help
 # ./rabbitmqadmin -H ${RABBITMQ_HOSTNAME} -u ${RABBITMQ_USERNAME} -p ${RABBITMQ_PASSWORD} list queues
