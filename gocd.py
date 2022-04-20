a38e092ebccb1006ba29fb288b76f72024e28569
Bernard Choi <bchoii@gmail.com>
2022-04-20 16:39:36 +0800

    .

--- gocd.py
+++ gocd.py
@@ -18,7 +18,9 @@ output = open("purge.sh", "w")
 # Iterating through the json list
 for row in data:
   print(row)
-  output.write("./rabbitmqadmin -H ${CLOUD_RABBITMQ_HOSTNAME} -u ${CLOUD_RABBITMQ_USERNAME} -p ${CLOUD_RABBITMQ_PASSWORD} --vhost={0} purge queue name={1}".format(row['vhost'], row['name']))
+  print(row['vhost'])
+  print(row['name'])
+  # output.write("./rabbitmqadmin -H ${CLOUD_RABBITMQ_HOSTNAME} -u ${CLOUD_RABBITMQ_USERNAME} -p ${CLOUD_RABBITMQ_PASSWORD} --vhost={} purge queue name={}".format(row['vhost'], row['name']))
 
 output.close()
 
