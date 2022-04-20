9dd6e7f6b656df3b7d8a2d0d456ac10be1b999c1
Bernard Choi <bchoii@gmail.com>
2022-04-20 16:43:38 +0800

    .

--- gocd.py
+++ gocd.py
@@ -20,7 +20,7 @@ for row in data:
   print(row)
   print(row['vhost'])
   print(row['name'])
-  # output.write("./rabbitmqadmin -H ${CLOUD_RABBITMQ_HOSTNAME} -u ${CLOUD_RABBITMQ_USERNAME} -p ${CLOUD_RABBITMQ_PASSWORD} --vhost={} purge queue name={}".format(row['vhost'], row['name']))
+  output.write("./rabbitmqadmin -H ${CLOUD_RABBITMQ_HOSTNAME} -u ${CLOUD_RABBITMQ_USERNAME} -p ${CLOUD_RABBITMQ_PASSWORD} --vhost={} purge queue name={}".format(row['vhost'], row['name']))
 
 output.close()
 
