df6694a08bc6ebe5d8053563202d407d995eebd7
Bernard Choi <bchoii@gmail.com>
2022-04-20 16:16:31 +0800

    .

--- gocd.sh
+++ gocd.sh
@@ -1,12 +1,14 @@
+cat queues.json
+# ./rabbitmqadmin --help
+# curl http://${CLOUD_RABBITMQ_HOSTNAME}:15672/cli/rabbitmqadmin -o rabbitmqadmin
+# chmod +x rabbitmqadmin
+# cat ./rabbitmqadmin
 # echo ${CLOUD_RABBITMQ_HOSTNAME}
 # echo ${CLOUD_RABBITMQ_USERNAME}
 # echo ${CLOUD_RABBITMQ_PASSWORD}
 # echo ${VHOST}
 # echo ${QUEUE_NAME}
-curl http://${CLOUD_RABBITMQ_HOSTNAME}:15672/cli/rabbitmqadmin -o rabbitmqadmin
-chmod +x rabbitmqadmin
-# cat ./rabbitmqadmin
-./rabbitmqadmin --help
+# ./rabbitmqadmin -H ${CLOUD_RABBITMQ_HOSTNAME} -u ${CLOUD_RABBITMQ_USERNAME} -p ${CLOUD_RABBITMQ_PASSWORD} --vhost=${VHOST} purge queue name=${QUEUE_NAME}
 # ./rabbitmqadmin -H ${RABBITMQ_HOSTNAME} -u ${RABBITMQ_USERNAME} -p ${RABBITMQ_PASSWORD} list queues
 # ./rabbitmqadmin -H ${RABBITMQ_HOSTNAME} -u ${RABBITMQ_USERNAME} -p ${RABBITMQ_PASSWORD} -f raw_json list queues > queues.json
 # cat queues.json
